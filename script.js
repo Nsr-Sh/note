96998c5fe1711e5ab5f7ac9955feeaeffaf78a9e
nsrsh25 <108688245+nsrsh25@users.noreply.github.com>
2022-12-07 22:24:58 +0330

    start
    html, css

---
+++ script.js
@@ -0,0 +1,11 @@
+// variables
+
+const box = document.querySelector('#main .container'),
+  stickyNote = document.querySelector('.stickynotes'),
+  title = document.querySelector('.stickynotes>input'),
+  text = document.querySelector('.stickynotes>textarea'),
+  palette = document.querySelector('.colorpalette'),
+  iconPalette = document.querySelector('.fa-palette'),
+  iconEdit = document.querySelector('.fa-pencil'),
+  iconNew = document.querySelector('.fa-check'),
+  iconDelete = document.querySelector('.fa-trash-can');
